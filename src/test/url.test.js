0b936bfed1647025b487601206554934dd4dbb58
abidex4yemi <abidex4yemi@gmail.com>
2023-10-04 23:16:40 +0100

    Chore: add invalid short id test

--- src/test/url.test.js
+++ src/test/url.test.js
@@ -48,6 +48,25 @@ describe('GET /api/v1/url/decode', () => {
   });
 });
 
+describe('GET /api/v1/url/decode', () => {
+  it('should return error if short id not valid', async () => {
+    const urlExistResponse = await new Promise((resolve, reject) => {
+      request(app)
+        .get(`${baseUrl}/decode/random123`)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(404)
+        .end((err, res) => {
+          if (err) return reject(err);
+          resolve(res.body);
+        });
+    });
+    expect(urlExistResponse.success).to.be.false;
+    expect(urlExistResponse.content.shortUrl).to.not.exist;
+    expect(urlExistResponse.message).to.equal('url not found');
+  });
+});
+
 describe('GET /api/v1/url/statistic', () => {
   it('should get short Url statistics', async () => {
     const urlExistResponse = await new Promise((resolve, reject) => {
